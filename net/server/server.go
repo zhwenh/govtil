1b4312e09bbf79f758f1a36cf5729f078e6c1f09
Vivek Sekhar <vivek@viveksekhar.ca>
2012-09-18 21:54:44 -0700

    Additional server functionality

--- net/server/server.go
+++ net/server/server.go
@@ -6,17 +6,18 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	_ "net/http/pprof"
 	"net/rpc"
 	"os"
 
 	"github.com/vsekhar/govtil/log"
 	vnet "github.com/vsekhar/govtil/net"
+	"github.com/vsekhar/govtil/net/server/birpc"
+	"github.com/vsekhar/govtil/net/server/direct"
 	"github.com/vsekhar/govtil/net/server/healthz"
 	"github.com/vsekhar/govtil/net/server/logginghandler"
-	"github.com/vsekhar/govtil/net/server/varz"
 	"github.com/vsekhar/govtil/net/server/streamz"
-	"github.com/vsekhar/govtil/net/server/direct"
-	"github.com/vsekhar/govtil/net/server/birpc"
+	"github.com/vsekhar/govtil/net/server/varz"
 )
 
 // TODO: testing using net/http/httptest
@@ -54,13 +55,21 @@ func RegisterRPCName(name string, rcvr interface{}) error {
 
 // placeholder request handler
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>govtil/server %s!</h1>", r.URL.Path[1:])
+	fmt.Fprintf(w, "<h1>govtil/net/server %s!</h1>", r.URL.Path[1:])
 }
 
 // Serve on a given port
 //
 // The server will log to the default logger and will gracefully terminate on
 // receipt of an os.Interrupt.
+//
+// The following URLs are defined:
+//    /
+//    /healthz
+//    /varz
+//    /streamz
+//    /birpc
+//    /debug/pprof
 //
 func ServeForever(port int) (err error) {
 	defer func() {
@@ -69,27 +78,31 @@ func ServeForever(port int) (err error) {
 		}
 	}()
 
-	mux := http.NewServeMux()
-	mux.Handle("/", logginghandler.New(http.HandlerFunc(defaultHandler), log.NORMAL))
-	mux.Handle("/healthz", logginghandler.New(Healthz, log.NORMAL))
-	mux.Handle("/varz", logginghandler.New(Varz, log.NORMAL))
+	http.Handle("/", logginghandler.New(http.HandlerFunc(defaultHandler), log.NORMAL))
+	http.Handle("/healthz", logginghandler.New(Healthz, log.NORMAL))
+	http.Handle("/varz", logginghandler.New(Varz, log.NORMAL))
 
 	// birpc
 	birpcconns := make(chan net.Conn)
-	mux.Handle("/birpc", logginghandler.New(&direct.Handler{birpcconns}, log.NORMAL))
+	http.Handle("/birpc", logginghandler.New(&direct.Handler{birpcconns}, log.NORMAL))
 	go birpc.DispatchForever(birpcconns, RPC, RPCClientsCh)
 
 	// streamz
 	subs := make(chan net.Conn)
-	mux.Handle("/streamz", &direct.Handler{subs})
+	http.Handle("/streamz", &direct.Handler{subs})
 	go streamz.DispatchForever(subs, StreamzCh)
 	go streamz.Ticker(StreamzCh)
 
 	addr := ":" + fmt.Sprint(port)
 	l, err := vnet.Listen("tcp", addr, os.Interrupt)
-	if err != nil { return }
+	if err != nil {
+		log.Println("govtil/net/server: Failed to listen on", port, err)
+		return
+	}
 
-	srv := &http.Server{Addr: addr, Handler: mux}
-	err = srv.Serve(l)
+	err = http.Serve(l, nil)
+	if err != nil {
+		log.Println("govtil/net/server: Server terminated with error", err)
+	}
 	return
 }
