029ed540970a4f410ae93a7a7efeaa4693e4c704
Vivek Sekhar <vivek@viveksekhar.ca>
2012-09-19 21:11:21 -0700

    Add Always*() and Error*(), gofmt

--- log/log.go
+++ log/log.go
@@ -12,12 +12,13 @@ import (
 var verbosity = flag.Int("verbosity", 0, "logging verbosity (-1==QUIET to 1==DEBUG)")
 
 const (
-	QUIET = -1
+	QUIET  = -1
 	NORMAL = 0
-	DEBUG = 1
+	DEBUG  = 1
 )
 
 type Level int
+
 var mutex sync.RWMutex
 
 func test(level Level) bool {
@@ -32,19 +33,68 @@ func SetVerbosity(level Level) {
 	*verbosity = int(level)
 }
 
-func Log(level Level, args ...interface{}) { if test(level) { stdlog.Print(args...) } }
-func Logf(level Level, s string, args ...interface{}) { if test(level) { stdlog.Printf(s, args...) } }
-func Logln(level Level, args ...interface{}) { if test(level) { stdlog.Println(args...) } }
-
-func Debug(args ...interface{}) { if test(DEBUG) { stdlog.Print(args...) } }
-func Debugf(s string, args ...interface{}) { if test(DEBUG) { stdlog.Printf(s, args...) } }
-func Debugln(args ...interface{}) { if test(DEBUG) { stdlog.Println(args...) } }
-
-func Print(args ...interface{}) { if test(NORMAL) { stdlog.Print(args...) } }
-func Printf(s string, args ...interface{}) { if test(NORMAL) { stdlog.Printf(s, args...) } }
-func Println(args ...interface{}) { if test(NORMAL) { stdlog.Println(args...) } }
-
-// Always log fatals
+// Not silenceable, terminates
 var Fatal = stdlog.Fatal
 var Fatalf = stdlog.Fatalf
 var Fatalln = stdlog.Fatalln
+
+// Not silenceable, does not terminate
+func Always(args ...interface{})            { stdlog.Print(args...) }
+func Alwaysf(s string, args ...interface{}) { stdlog.Printf(s, args...) }
+func Alwaysln(args ...interface{})          { stdlog.Println(args...) }
+
+// Not silenceable, does not terminate (might revise this in the future)
+var Error = Always
+var Errorf = Alwaysf
+var Errorln = Alwaysln
+
+// Debug level
+func Debug(args ...interface{}) {
+	if test(DEBUG) {
+		stdlog.Print(args...)
+	}
+}
+func Debugf(s string, args ...interface{}) {
+	if test(DEBUG) {
+		stdlog.Printf(s, args...)
+	}
+}
+func Debugln(args ...interface{}) {
+	if test(DEBUG) {
+		stdlog.Println(args...)
+	}
+}
+
+// Normal level
+func Print(args ...interface{}) {
+	if test(NORMAL) {
+		stdlog.Print(args...)
+	}
+}
+func Printf(s string, args ...interface{}) {
+	if test(NORMAL) {
+		stdlog.Printf(s, args...)
+	}
+}
+func Println(args ...interface{}) {
+	if test(NORMAL) {
+		stdlog.Println(args...)
+	}
+}
+
+// Specified level
+func Log(level Level, args ...interface{}) {
+	if test(level) {
+		stdlog.Print(args...)
+	}
+}
+func Logf(level Level, s string, args ...interface{}) {
+	if test(level) {
+		stdlog.Printf(s, args...)
+	}
+}
+func Logln(level Level, args ...interface{}) {
+	if test(level) {
+		stdlog.Println(args...)
+	}
+}
