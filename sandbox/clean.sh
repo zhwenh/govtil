5b5275d928e6d446f6b86bbba461f9c0cd13702e
Vivek Sekhar <vivek@viveksekhar.ca>
2012-10-18 23:48:06 -0700

    Update .gitignore to keep only src and license stuff.

--- sandbox/clean.sh
+++ sandbox/clean.sh
@@ -3,7 +3,7 @@
 pwd=`pwd`
 
 shopt -s extglob
-cd $pwd/go && rm -rf api doc include lib misc test AUTHORS CONTRIBUTORS favicon.ico LICENSE PATENTS README robots.txt
+cd $pwd/go && rm -rf api doc include lib misc test favicon.ico robots.txt
 cd $pwd/go/bin && rm -f !(go)
 
 # pkg dir
