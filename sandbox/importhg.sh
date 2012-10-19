61653f77904dae14fc0f2cc021365cc5d3ede534
Vivek Sekhar <vivek@viveksekhar.ca>
2012-10-18 23:41:49 -0700

    Handle repository reset

--- sandbox/importhg.sh
+++ sandbox/importhg.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-hg clone -r release -b default -u release https://code.google.com/p/go
+if [ -d "go/.hg" ]; then
+	hg -R go update --clean -r release
+else
+	hg clone -r release -b default -u release https://code.google.com/p/go
+fi
+
 cd go/src
 . ./all.bash
