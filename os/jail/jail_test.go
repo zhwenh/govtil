f7a0083b6209c4759ac31de61e87897b9cdbf044
Vivek Sekhar <vivek@viveksekhar.ca>
2013-03-30 23:57:23 -0700

    Network support

--- os/jail/jail_test.go
+++ os/jail/jail_test.go
@@ -2,8 +2,7 @@ package jail
 
 import (
 	"os"
-	"os/exec"
-	"strings"
+	"os/signal"
 	"testing"
 )
 
@@ -21,7 +20,7 @@ func TestFaked(t *testing.T) {
 // TODO(vsekhar): test NewLxcJail()
 
 func TestLxcJail(t *testing.T) {
-	lxc, err := NewLxcJail("/home/vsekhar/chroot")
+	lxc, err := NewLxcJail("/home/vsekhar/chroot", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -32,16 +31,30 @@ func TestLxcJail(t *testing.T) {
 	}()
 
 	// cmd := exec.Command("ifconfig")
-	cmd := exec.Command("ping", "74.125.224.136") // google.com
-	// cmd := exec.Command("pwd")
+	// cmd := exec.Command("bash", "-c", "echo hello; echo goodbye")
+	// cmd := exec.Command("bash", "-c", "env")
+	// cmd := exec.Command("ping", "-c", "1", "74.125.224.105")
+	cmd := Command("ping google.com")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 	if cmd, err = lxc.Imprison(cmd); err != nil {
 		t.Fatal(err)
 	}
-	t.Logf("Commandline: %s", strings.Join(cmd.Args, " "))
-	if err = cmd.Run(); err != nil {
+	if err = cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Forward Ctrl-C to underlying process, to allow Go to cleanup
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for s := range c {
+			cmd.Process.Signal(s)
+		}
+	}()
+	if err := cmd.Wait(); err != nil {
 		t.Error(err)
 	}
+
 }
--- os/jail/lxc.go
+++ os/jail/lxc.go
@@ -6,19 +6,20 @@ package jail
 import (
 	"errors"
 	"fmt"
+	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"text/template"
 
 	guidlib "github.com/vsekhar/govtil/guid"
 )
 
-// taken from github.com/dotcloud/docker/lxc_template.go@d1ed28cb941578b07420a507886ddebaed78deb8
 const raw_lxc_template = `
 # hostname
-lxc.utsname = govtil_jail_{{.ID}}
+lxc.utsname = {{.ID}}
 #lxc.aa_profile = unconfined
 
 # network configuration
@@ -30,6 +31,7 @@ lxc.network.link = {{.BridgeID}}
 lxc.network.mtu = 1500
 lxc.network.hwaddr = 00:11:22:33:44:d
 #lxc.network.ipv4 = 192.168.1.2/24
+#lxc.network.ipv4 = 10.0.0.2/24
 lxc.network.ipv4 = 0.0.0.0
 
 # root filesystem
@@ -75,7 +77,6 @@ lxc.cgroup.devices.allow = c 10:200 rwm
 # rtc
 #lxc.cgroup.devices.allow = c 254:0 rwm
 
-
 # standard mount point
 lxc.mount.entry = proc {{.RootFS}}/proc proc nosuid,nodev,noexec 0 0
 lxc.mount.entry = sysfs {{.RootFS}}/sys sysfs nosuid,nodev,noexec 0 0
@@ -91,7 +92,7 @@ lxc.mount.entry = devpts {{.RootFS}}/dev/pts devpts newinstance,ptmxmode=0666,no
 #lxc.mount.entry = /usr/lib/lxc/lxc-init {{.RootFS}}/usr/lib/lxc/lxc-init none bind,ro 0 0
 
 # In order to get a working DNS environment, mount bind (ro) the host's /etc/resolv.conf into the container
-lxc.mount.entry = /etc/resolv.conf {{.RootFS}}/etc/resolv.conf none bind,ro 0 0
+# lxc.mount.entry = /etc/resolv.conf {{.RootFS}}/etc/resolv.conf none bind,ro 0 0
 
 # drop linux capabilities (apply mainly to the user root in the container)
 lxc.cap.drop = audit_control audit_write
@@ -119,14 +120,30 @@ lxc.cgroup.cpu.shares = {{.CPU}}
 {{end}}
 `
 
+const cmdTemplate = `
+#ifconfig eth0 10.0.0.2 netmask 255.0.0.0 up
+#route add -host 10.0.0.1 dev eth0
+#route add default gw 10.0.0.1 eth0
+ip link set eth0 up
+ip addr add 10.0.0.2/24 dev eth0
+ip route add default via 10.0.0.1 dev eth0
+#hostname; ifconfig; ip route show
+echo > /etc/resolv.conf nameserver 8.8.8.8
+echo >> /etc/resolv.conf nameserver 8.8.4.4
+echo >> /etc/resolv.conf search govtil_jail
+# user command on next line
+%s
+`
+
 const MEG = 1024 * 1024
 
 const ID_PREFIX = "govtil_jail_"
+const extID = "wlan0" // 'real' external host network interface
 
 var lxcTemplate *template.Template
 
 func init() {
-	// Compile lxc.conf template (use with lxcTemplate.Execute(out, config))
+	// Compile lxc.conf template (use with template.Execute(out, config))
 	var err error
 	lxcTemplate, err = template.New("lxc").Parse(raw_lxc_template)
 	if err != nil {
@@ -145,9 +162,12 @@ type lxcjail struct {
 }
 
 func (l *lxcjail) Imprison(c *exec.Cmd) (*exec.Cmd, error) {
+	if len(c.Args) > 0 {
+		return nil, fmt.Errorf("Error, cannot have Args in cmd. Must be combined into a single Bash-parsable string in cmd.Path")
+	}
+	user_cmd := c.Path
 	cmd := new(exec.Cmd)
 	*cmd = *c
-	original_path := cmd.Path
 	// lxc-execute doesn't work because it wants to mount /proc and requires
 	// CAP_SYS_ADMIN to do so, but we drop that capability in the conf file
 	cmd.Path = "/usr/bin/lxc-start"
@@ -155,9 +175,10 @@ func (l *lxcjail) Imprison(c *exec.Cmd) (*exec.Cmd, error) {
 		cmd.Path,
 		fmt.Sprintf("--name=%s", l.ID),
 		fmt.Sprintf("--rcfile=%s", filepath.Join(os.TempDir(), l.ID)),
-		"--",
-		original_path,
-	}, cmd.Args[1:]...)
+		"--", "bash", "-c",
+	})
+	script := fmt.Sprintf(cmdTemplate, user_cmd)
+	cmd.Args = append(cmd.Args, script)
 
 	return cmd, nil
 }
@@ -182,7 +203,21 @@ func (l *lxcjail) CleanUp() error {
 
 	// 2. Remove iptables and NAT routing
 	// TODO: remove NAT routing
-	accErrs(&errtext, iptables("-t", "nat", "-D", "POSTROUTING", "-j", "MASQUERADE", "-o", "wlan0"))
+	/*
+		if l.Ports != nil {
+			rports := make([]uint, len(l.Ports))
+			copy(rports, l.Ports)
+			for i, j := 0, len(rports)-1; i < j; i, j = i+1, j-1 {
+				rports[i], rports[j] = rports[j], rports[i]
+			}
+		}
+	*/
+
+	accErrs(&errtext, iptables("-D", "FORWARD", "-i", l.BridgeID, "-o", extID,
+		"-j", "ACCEPT"))
+	accErrs(&errtext, iptables("-D", "FORWARD", "-i", extID, "-o", l.BridgeID,
+		"-m", "state", "--state", "RELATED,ESTABLISHED", "-j", "ACCEPT"))
+	accErrs(&errtext, iptables("-t", "nat", "-D", "POSTROUTING", "-j", "MASQUERADE", "-o", extID))
 	accErrs(&errtext, iptables("-t", "nat", "-D", "OUTPUT", "-j", l.ID))
 	accErrs(&errtext, iptables("-t", "nat", "-D", "PREROUTING", "-j", l.ID))
 	accErrs(&errtext, iptables("-t", "nat", "-F", l.ID))
@@ -205,7 +240,7 @@ func (l *lxcjail) CleanUp() error {
 //  2. Create a new network bridge
 //  3. Setup iptables and NAT routing for requested ports
 //  4. Generate and write lxc configuration file
-func NewLxcJail(root string) (Interface, error) {
+func NewLxcJail(root string, ports []uint) (Interface, error) {
 	l := new(lxcjail)
 
 	// 1. Unique ID
@@ -223,11 +258,11 @@ func NewLxcJail(root string) (Interface, error) {
 	if err := brctl("addbr", l.BridgeID); err != nil {
 		return nil, err
 	}
-	if err := ifconfig(l.BridgeID, "10.0.0.7", "netmask", "255.0.0.0", "up"); err != nil {
+	if err := ifconfig(l.BridgeID, "10.0.0.1", "netmask", "255.0.0.0", "up"); err != nil {
 		return nil, err
 	}
 
-	// 3. Setup iptables
+	// 3. Setup iptables...
 	if err := iptables("-t", "nat", "-N", l.ID); err != nil {
 		return nil, fmt.Errorf("Failed to create %s chain: %s", l.ID, err)
 	}
@@ -237,11 +272,35 @@ func NewLxcJail(root string) (Interface, error) {
 	if err := iptables("-t", "nat", "-A", "OUTPUT", "-j", l.ID); err != nil {
 		return nil, fmt.Errorf("Failed to inject %s in OUTPUT chain: %s", l.ID, err)
 	}
-	if err := iptables("-t", "nat", "-A", "POSTROUTING", "-j", "MASQUERADE", "-o", "wlan0"); err != nil {
+
+	// ... IP re-write
+	if err := iptables("-t", "nat", "-A", "POSTROUTING", "-j", "MASQUERADE", "-o", extID); err != nil {
 		return nil, fmt.Errorf("Failed to enable MASQUERADE in POSTROUTING chain: %s", err)
 	}
+	// ... inbound forwarding (filtered)
+	if err := iptables("-A", "FORWARD", "-i", extID, "-o", l.BridgeID,
+		"-m", "state", "--state", "RELATED,ESTABLISHED", "-j", "ACCEPT"); err != nil {
+		return nil, fmt.Errorf("Failed to enable FORWARD from %s to %s: %s", extID, l.BridgeID, err)
+	}
+	// ... output forwarding
+	if err := iptables("-A", "FORWARD", "-i", l.BridgeID, "-o", extID,
+		"-j", "ACCEPT"); err != nil {
+		return nil, fmt.Errorf("Failed to enable FORWARD from %s to %s: %s", l.BridgeID, extID, err)
+	}
 
-	// TODO: Setup NAT using l.Ports
+	// TODO: Port forwarding
+	/*
+		l.Ports = ports
+		if l.Ports != nil {
+			for port := range l.Ports {
+				if err:= iptables("-t", "nat", "-A", "l.ID", "-p", "tcp", "--dport",
+					strconv.Itoa(port), "-j", "DNAT", "--to-destination",
+					net.JoinHostPort(dest.IP.String(), strconv.Itoa(dest.Port))); err != nil {
+					return nil, err
+				}
+			}
+		}
+	*/
 
 	// 4. Generate configuration file
 	cfilename := filepath.Join(os.TempDir(), l.ID)
@@ -255,8 +314,50 @@ func NewLxcJail(root string) (Interface, error) {
 	return l, nil
 }
 
+// Utility functions
+
+// Return the IPv4 address of a network interface
+func getIfaceAddr(name string) (net.Addr, error) {
+	iface, err := net.InterfaceByName(name)
+	if err != nil {
+		return nil, err
+	}
+	addrs, err := iface.Addrs()
+	if err != nil {
+		return nil, err
+	}
+	var addrs4 []net.Addr
+	for _, addr := range addrs {
+		ip := (addr.(*net.IPNet)).IP
+		if ip4 := ip.To4(); len(ip4) == net.IPv4len {
+			addrs4 = append(addrs4, addr)
+		}
+	}
+	switch {
+	case len(addrs4) == 0:
+		return nil, fmt.Errorf("Interface %v has no IP addresses", name)
+	case len(addrs4) > 1:
+		fmt.Printf("Interface %v has more than 1 IPv4 address. Defaulting to using %v\n",
+			name, (addrs4[0].(*net.IPNet)).IP)
+	}
+	return addrs4[0], nil
+}
+
+// TODO: re-do for ip * commands rather than iptables, route, etc.
+// see: http://dougvitale.wordpress.com/2011/12/21/deprecated-linux-networking-commands-and-their-replacements/#route
+
 func iptables(args ...string) error { return cmd("iptables", args...) }
-func brctl(args ...string) error { return cmd("brctl", args...) }
+func iptablesForward(rule string, chain string, port int, dest net.TCPAddr) error {
+	return iptables(
+		"-t", "nat", rule, chain,
+		"-p", "tcp",
+		"--dport", strconv.Itoa(port),
+		"-j", "DNAT",
+		"--to-destination", net.JoinHostPort(dest.IP.String(), strconv.Itoa(dest.Port)),
+	)
+}
+
+func brctl(args ...string) error    { return cmd("brctl", args...) }
 func ifconfig(args ...string) error { return cmd("ifconfig", args...) }
 
 // command wrapper
