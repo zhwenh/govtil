39720aea8aa872e63f969bf8af4c7aed51b29e5a
Vivek Sekhar <vivek@viveksekhar.ca>
2015-07-26 00:58:15 -0700

    Socket listener that is closed when signal is received.

--- net/net.go
+++ net/net.go
@@ -2,8 +2,14 @@
 package net
 
 import (
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"strings"
+
+	"github.com/vsekhar/govtil/log"
+	"github.com/vsekhar/govtil/os/signal"
 )
 
 // Return whether the given error indicates a socket that produced it has been
@@ -32,4 +38,23 @@ func SocketClosed(err error) bool {
 		return true
 	}
 	return false
+}
+
+// Signallistener returns a net.Listener that will be closed when standard OS
+// stop signals are received.
+func SignalListener(port int) (net.Listener, error) {
+	addr := ":" + fmt.Sprint(port)
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Errorln("govtil/net: Failed to listen on", port, err)
+		return nil, err
+	}
+
+	// Close listen port on signals (causes http.Serve() to return)
+	signal.Go(func(s os.Signal) {
+		log.Debugf("govtil/net: Closing listen port %v due to signal %v", l.Addr().String(), s)
+		l.Close()
+	})
+
+	return l, nil
 }
