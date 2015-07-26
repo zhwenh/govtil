1f08565067608cfd3e4f7b005b60dfcb6fd0f4af
Vivek Sekhar <vivek@viveksekhar.ca>
2015-07-26 00:59:26 -0700

    Combine ReadCloser and WriteCloser into a ReadWriteCloser

---
+++ io/io.go
@@ -0,0 +1,34 @@
+package io
+
+import (
+	stdio "io"
+)
+
+type rwc struct {
+	stdio.ReadCloser
+	stdio.WriteCloser
+}
+
+func (this *rwc) Close() error {
+	ec := make(chan error)
+	go func() {
+		if err := this.ReadCloser.Close(); err != nil {
+			ec <- err
+		}
+	}()
+
+	if err := this.WriteCloser.Close(); err != nil {
+		return err
+	}
+	if err := <-ec; err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewReadWriteCloser combines an io.ReadCloser with an io.WriteCloser. When the
+// returned io.ReadWriteCloser is closed, both the ReadCloser and WriteCloser
+// are closed in parallel.
+func NewReadWriteCloser(r stdio.ReadCloser, w stdio.WriteCloser) stdio.ReadWriteCloser {
+	return &rwc{r, w}
+}
